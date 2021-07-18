537a45191b22a2b3b050c0eb4da493d652a5847a
Ondrej Kucera <ondrej.kucera.88@gmail.com>
2021-07-18 13:36:40 +0200

    Added 'style' prop

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,11 +2,11 @@ import React from "react";
 import useComponentLog from "../hooks/useComponentLog";
 
 // Using object destructuring instead of props
-function Tile({ heading, text }) {
+function Tile({ heading, text, style }) {
   useComponentLog("Tile");
 
   return (
-    <li className="Tile">
+    <li className="Tile" style={style}>
       <h4 className="Tile-heading">{heading}</h4>
       {text ? <p className="Tile-text">{text}</p> : ""}
     </li>
